11d510074481e0bdceed9c1b44b0b42bd44b8bbf
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-07-28 17:41:10 +0100

    Rideriect, Switch,Route URL params and querystring

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import HomePage from "./HomePage"
 import About from "./About.js"
 import Header from "./common/Header"
 import CoursePage from "./CoursePage"
-import {Route} from "react-router-dom"
+import {Route, Switch, Redirect} from "react-router-dom"
+import NotFoundPage from "./NotFoundPage"
+import ManageCoursePage from "./ManageCoursePage"
 
 
 export default function App(){
@@ -11,9 +13,14 @@ export default function App(){
     return(
         <div className="container-fluid">
             <Header/>
-            <Route path="/" exact component={HomePage}/>
-            <Route path="/about" component={About}/>
-            <Route path="/courses" component={CoursePage}/>
+            <Switch>
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/about" component={About}/>
+                <Route path="/courses" component={CoursePage}/>
+                <Route path="/course" component={ManageCoursePage}
+                <Redirect from="/about-page" to="/about" />
+                <Route component={NotFoundPage}/>
+            </Switch>
         </div>
     )
 }
---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ManageCoursePage= props => {
+    return(
+        <>
+        <h1>Manage Course Page <h1>
+        {props.match.slug}
+        </>
+    )
+}
+
+export default: CourseManagePage
